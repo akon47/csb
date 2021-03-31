e5b30ae1eca35b66ca68abfdb6e40c22b2d0bfb6
kimhwan <akon47@naver.com>
2021-03-31 20:21:45 +0900

    Update csb.py

--- csb.py
+++ csb.py
@@ -52,8 +52,16 @@ class Tree:
         
     def distance(self, other):
         return math.sqrt(pow(other.x - self.x, 2) + pow(other.y - self.y, 2))
+
+def getValidByMode(mode,v,range1,range2):
+    if mode =='a':
+          return True if v >= range1 else False
+    elif mode =='b':
+          return True if v <= range1 else False
+    else:
+        return True if range1 <= v and v <= range2 else False
     
-def find_all_mt(thread_value, distance, count, max_result, trees, bag, start, above):
+def find_all_mt(thread_value, count, max_result, trees, bag, start, mode, range1, range2):
     if max_result <= thread_value.value:
         return True
     
@@ -66,52 +74,21 @@ def find_all_mt(thread_value, distance, count, max_result, trees, bag, start, ab
     for i in range(start, len(trees)):
         is_valid = True
         for value in bag:
-            if above:
-                if(trees[i].distance(value) < distance):
-                    is_valid = False
-                    break
-            else:
-                if(trees[i].distance(value) > distance):
-                    is_valid = False
-                    break
-        if is_valid:
-            new_bag = list(bag)
-            new_bag.append(trees[i])
-            if len(new_bag) > 20:
-                thread = Thread(target=find_all_mt, args=(thread_value, distance, count, max_result, trees, new_bag, i + 1, above,))
-                thread.start()
-            else:
-                find_all_mt(thread_value, distance, count, max_result, trees, new_bag, i + 1, above)
-        new_bag = list(bag)
-    
-    return
-    
-def find_all(thread_value, distance, count, max_result, trees, bag, start):
-    if(len(bag) == count):
-        if thread_value.add(1) <= max_result:
-            print('\n-----------------------------')
-            for i in range(0, len(bag)):
-                print(bag[i].name, end='')
-                if (i + 1) % 8 != 0:
-                    print(', ', end='')
-                else:
-                    print('')
-        return
-    
-    for i in range(start, len(trees)):
-        is_valid = True
-        for value in bag:
-            if(trees[i].distance(value) < distance):
+            isCondition = getValidByMode(mode,trees[i].distance(value),range1,range2)
+            if not isCondition :
                 is_valid = False
                 break
         if is_valid:
             new_bag = list(bag)
             new_bag.append(trees[i])
-            find_all(thread_value, distance, count, max_result, trees, new_bag, i + 1)
+            if len(new_bag) > 20:
+                thread = Thread(target=find_all_mt, args=(thread_value, count, max_result, trees, new_bag, i + 1, mode, range1, range2,))
+                thread.start()
+            else:
+                find_all_mt(thread_value, count, max_result, trees, new_bag, i + 1, mode, range1, range2)
         new_bag = list(bag)
     
-    return
-            
+    return            
 
 def main():    
     file = open(sys.argv[1], 'r')
@@ -127,37 +104,30 @@ def main():
         trees.append(Tree(p[0][0], count, p[0][1], p[0][2]))
         count += 1
         
-    distance = 15
-    number_of_trees = 20
-    max_result = 30
-    above = True
-    shuffle = False
-    
-    if len(sys.argv) > 2:
-        distance = int(sys.argv[2])
-    if len(sys.argv) > 3:
-        number_of_trees = int(sys.argv[3])
-    if len(sys.argv) > 4:
-        max_result = int(sys.argv[4])
-    if len(sys.argv) > 5:
-        if sys.argv[5] == 'y':
-            shuffle = True
-    if len(sys.argv) > 6:
-        if sys.argv[6] == 'b':
-            above = False
+    mode = input("모드 이상-a, 이하-b , 범위-c: ")
+    range1 = 0
+    range2 = 0
+    if mode =='a':
+        range1 = int(input("이상: "))
+    elif mode =='b':
+        range1 = int(input("이하: "))
+    elif mode =="c":
+        range1= int(input("이상: "))
+        range2= int(input("이하: "))
+
+    number_of_trees = int(input("위 조건을 만족하는 나무의 개수: "))
+    max_result = int(input("최대 몇 개의 결과를 출력할까요? "))
 
     threads = []
     
-    if shuffle:
-        for value in range(max_result):
-            random.shuffle(trees)
-            thread = Thread(target=find_all_mt, args=(ThreadVariable(), distance, number_of_trees, 1, list(trees), [], 0, above))
-            thread.start()
-            threads.append(thread)
-        for t in threads:
-            t.join()
-    else:
-        find_all_mt(ThreadVariable(), distance, number_of_trees, max_result, trees, [], 0, above)
+    for value in range(max_result):
+        random.shuffle(trees)
+        thread = Thread(target=find_all_mt, args=(ThreadVariable(), number_of_trees, 1, trees, [], 0, mode, range1, range2))
+        thread.start()
+        threads.append(thread)
+        
+    for t in threads:
+        t.join()
     
 if __name__ == "__main__":
     main()
